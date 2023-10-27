0698c25385afb8c6859af1de93bd5d24843ad7ab
Florin Hasna <florinhasna@outlook.com>
2023-10-27 20:43:17 +0100

    Fixed logic error, in the for loop. "i" was always taking the value of number as the last iteration, which makes the if statement true, and sets isPrime to false.

--- isPrime.cpp
+++ isPrime.cpp
@@ -8,7 +8,7 @@ int main(){
   // test divisors of number, if a divisor other than 1 and number is
   // found, then number is not prime.
   bool isPrime = true;
-  for (int i = 2; i <= number; i++)
+  for (int i = 2; i < number; i++)
     if(!(number % i))
       isPrime = false;
 
